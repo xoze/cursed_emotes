a808c983426bead5d9b2fd04a03e6babde6d8f8c
xoze <38741069+xoze@users.noreply.github.com>
2019-02-01 08:45:21 -0500

    initial commit

---
+++ curse.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function curse
+{
+  in="$1"
+  out="$2"
+  
+  tr "$in" "$out" < cursed_emotes.txt > cursed_emotes.tmp
+  
+  mv cursed_emotes.tmp cursed_emotes.txt
+}
+
+cp emotes.txt cursed_emotes.txt
+
+curse "a" "а"
+curse "e" "е"
+curse "i" "і"
+curse "o" "о"
+#curse "u" "⋃"
+
+curse "c" "с"
+curse "p" "р"
+curse "j" "ј"
+curse "y" "у"
+curse "x" "х"
+curse "v" "v"
+curse "m" "ⅿ"
+curse "g" "ɡ"
+
+while read emote
+do
+  cursed=`echo $emote | sed -e 's/:/꞉/'`
+  sed -e "s/$emote/$cursed/" < cursed_emotes.txt > cursed_emotes.tmp
+  mv cursed_emotes.tmp cursed_emotes.txt
+done < <(sort emotes.txt cursed_emotes.txt |uniq -c | grep "2" | sed -e 's/ *2 *//')
